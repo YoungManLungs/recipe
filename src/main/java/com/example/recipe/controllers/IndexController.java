d64dbd8a1f017b1998faa3d13d6044bd20284fc1
DoubleDurian <thedoubledurian@gmail.com>
2019-10-13 13:00:52 -0500

    adds one to one relationship for UnitOfMeasure

--- src/main/java/com/example/recipe/controllers/IndexController.java
+++ src/main/java/com/example/recipe/controllers/IndexController.java
@@ -1,13 +1,25 @@
 package com.example.recipe.controllers;
 
+import com.example.recipe.repositories.CategoryRepository;
+import com.example.recipe.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
+    private RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+        log.debug("He pinged me kramer, he pinged me, I can believe he pinged me!");
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
---
+++ src/main/java/com/example/recipe/services/RecipeServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.recipe.services;
+
+import com.example.recipe.domain.Recipe;
+import com.example.recipe.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
+@Service
+public class RecipeServiceImpl implements RecipeService{
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+
+    @Override
+    public Set<Recipe> getRecipes() {
+        log.debug("I'm in the server babay!");
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+        return recipeSet;
+    }
+}
